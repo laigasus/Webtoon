eb9c45ba04469edccb3e13656e40ba83732dbcce
Ok Jae Ook <okjaeook98@gmail.com>
2021-05-29 19:30:36 +0900

    모델 최신화

--- src/main/java/com/webtoon/mapper/BoardMapper.java
+++ src/main/java/com/webtoon/mapper/BoardMapper.java
@@ -10,31 +10,35 @@ import com.webtoon.domain.BoardVO;
 
 public interface BoardMapper {
 	// 글 등록 메서드
-	@Select("insert into toon_board values(null,?,?,?,?,?,?,?)")
+	@Select("insert into toon_board values(null,#{bd_title},#{bd_writer},#{bd_date},#{bd_view},#{bd_content},#{bd_img},#{bd_email})")
 	void regist(String email, String writer, String title, String content, String absoluteImgPath);
 
 	// 글 목록을 가지고 오는 메서드
-	@Select("SELECT * FROM toon_board WHERE bd_email !='root@naver.com' ORDER BY bd_num DESC LIMIT ?,?;")
+	// 원래 Sql "SELECT * FROM toon_board WHERE bd_email !='root@naver.com' ORDER BY bd_num DESC LIMIT ?,?;
+	@Select("SELECT * FROM toon_board WHERE bd_email !='root@naver.com' ORDER BY bd_num DESC LIMIT ${startRow},${pageSize};")
 	List<BoardVO> listBoard(int startRow,int pageSize);
 
 	// 내가 쓴 글 목록을 가지고 오는 메서드
-	@Select("select * from toon_board where bd_email ='".concat(email) + "' order by bd_num DESC;")
+	//원래 sql문 "select * from toon_board where bd_email ='".concat(email) + "' order by bd_num DESC;"
+	@Select("select * from toon_board where bd_email ='.concat(#{bd_email}) + ' order by bd_num DESC;")
 	List<BoardVO> myListBoard(String email);
 
 	// 글 상세보기 요청을 처리할 메서드
-	@Select("SELECT * FROM toon_board WHERE bd_num=?")
+	@Select("SELECT * FROM toon_board WHERE bd_num=#{bd_num}")
 	BoardVO contentBoard(int bId);
 
 	// 글 수정 요청을 처리할 메서드
-	@Update("UPDATE toon_board SET " + "bd_title=?, bd_content=?,bd_img=? " + "WHERE bd_num=?")
-	void updateBoard(String title, String content, String img, int bId);
+	@Update("UPDATE toon_board SET " + "bd_title=#{bd_title}, bd_content=#{bd_content},bd_img=#{bd_img} " + "WHERE bd_num=#{bd_num}")
+	void updateBoard(String bd_title, String bd_content, String absoluteImgPath, int bId);
 
 	// 글 삭제 요청을 처리할 메서드
-	@Delete("DELETE FROM toon_board WHERE bd_num=?")
+	@Delete("DELETE FROM toon_board WHERE bd_num=#{bd_num}")
 	void deleteBoard(int bId);
 
 	// 글 검색 요청을 처리할 메서드
-	@Select("SELECT * FROM toon_board WHERE bd_title LIKE ? AND bd_email !='root@naver.com'")
+	// SELECT * FROM toon_board WHERE bd_title LIKE ? AND bd_email !='root@naver.com'
+	// 
+	@Select("SELECT * FROM toon_board WHERE ${category} LIKE ${search} AND bd_email !='root@naver.com'")
 	List<BoardVO> searchBoard(String search, String category);
 
 	// 관리자의 글을 가지고 오는 메서드
@@ -42,7 +46,7 @@ public interface BoardMapper {
 	List<BoardVO> AdminListBoard();
 
 	// DB에 있는 view의 값을 증가 시켜주는 메서드
-	@Update("UPDATE toon_board SET " + "bd_view=? " + "WHERE bd_num=?")
+	@Update("UPDATE toon_board SET bd_view=${bView} WHERE bd_num=${bId}")
 	void viewIncrease(int bId, int bView);
 	
 	// 글갯수를 받아온다.
--- src/main/java/com/webtoon/service/BoardServiceImpl.java
+++ src/main/java/com/webtoon/service/BoardServiceImpl.java
@@ -33,153 +33,186 @@ public class BoardServiceImpl implements BoardService {
 			e.printStackTrace();
 		}
 	}
-
-
 	////////////////////////////////////////////////////////////////
 
+	//글등록 메서드
 	@Override
 	public void regist(String email, String writer, String title, String content, String absoluteImgPath) {
-
-		Timestamp time = new Timestamp(System.currentTimeMillis());
-		String sql = "insert into toon_board values(null,?,?,?,?,?,?,?)";
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setString(1, title);
-			pstmt.setString(2, writer);
-			pstmt.setTimestamp(3, time);
-			pstmt.setInt(4, 1);
-			pstmt.setString(5, content);
-			pstmt.setString(6, absoluteImgPath);
-			pstmt.setString(7, email);
-			pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+		boardMapper.regist(email, writer, title, content, absoluteImgPath);	
 	}
-	//startRow 부터 pageSize 갯수만큼 출력
+//	@Override
+//	public void regist(String email, String writer, String title, String content, String absoluteImgPath) {
+//		Timestamp time = new Timestamp(System.currentTimeMillis());
+//		String sql = "insert into toon_board values(null,?,?,?,?,?,?,?)";
+//		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//			pstmt.setString(1, title);
+//			pstmt.setString(2, writer);
+//			pstmt.setTimestamp(3, time);
+//			pstmt.setInt(4, 1);
+//			pstmt.setString(5, content);
+//			pstmt.setString(6, absoluteImgPath);
+//			pstmt.setString(7, email);
+//			pstmt.executeUpdate();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}	
+	//글 목록 원하는 범위 보여주기
 	@Override
 	public List<BoardVO> listBoard(int startRow,int pageSize) {
-		ResultSet rs;
-		List<BoardVO> articles = new ArrayList<>();
-		String sql = "SELECT * FROM toon_board WHERE bd_email !='root@naver.com' ORDER BY bd_num DESC LIMIT ?,?;";  
-		//LIMIT가 bd_num 값으로 가져오는게 아니라 정렬해서 값순서대로 가져옴 이거몰라서 4시간씀
-
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setInt(1, startRow);
-			pstmt.setInt(2, pageSize);
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				BoardVO article = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
-						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
-						rs.getInt("bd_view"));
-				articles.add(article);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return articles;
+		return boardMapper.listBoard(startRow, pageSize);
 	}
-
+	
+	//startRow 부터 pageSize 갯수만큼 출력
+//	@Override
+//	public List<BoardVO> listBoard(int startRow,int pageSize) {
+//		ResultSet rs;
+//		List<BoardVO> articles = new ArrayList<>();
+//		String sql = "SELECT * FROM toon_board WHERE bd_email !='root@naver.com' ORDER BY bd_num DESC LIMIT ?,?;";  
+//		//LIMIT가 bd_num 값으로 가져오는게 아니라 정렬해서 값순서대로 가져옴 이거몰라서 4시간씀
+//
+//		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//			pstmt.setInt(1, startRow);
+//			pstmt.setInt(2, pageSize);
+//			rs = pstmt.executeQuery();
+//
+//			while (rs.next()) {
+//				BoardVO article = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
+//						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
+//						rs.getInt("bd_view"));
+//				articles.add(article);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return articles;
+//	}
+	// 내가쓴 글만 select
 	@Override
 	public List<BoardVO> myListBoard(String email) {
-
-		List<BoardVO> datas = new ArrayList<>();
-		String sql = "select * from toon_board where bd_email ='".concat(email) + "' order by bd_num DESC;";
-
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				BoardVO post = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
-						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
-						rs.getInt("bd_view"));
-				datas.add(post);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return datas;
+		return boardMapper.myListBoard(email);
 	}
+	
+//	@Override
+//	public List<BoardVO> myListBoard(String email) {
+//
+//		List<BoardVO> datas = new ArrayList<>();
+//		String sql = "select * from toon_board where bd_email ='".concat(email) + "' order by bd_num DESC;";
+//
+//		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//			ResultSet rs = pstmt.executeQuery();
+//			while (rs.next()) {
+//				BoardVO post = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
+//						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
+//						rs.getInt("bd_view"));
+//				datas.add(post);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return datas;
+//	}
 
 	@Override
 	public BoardVO contentBoard(int bId) {
-
-		BoardVO vo = null;
-		String sql = "SELECT * FROM toon_board WHERE bd_num=?";
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setInt(1, bId);
-			ResultSet rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				vo = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
-						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
-						rs.getInt("bd_view"));
-			}
-		} catch (Exception e) {
-
-		}
-		return vo;
+		return boardMapper.contentBoard(bId);
 	}
+		
+//	@Override
+//	public BoardVO contentBoard(int bId) {
+//
+//		BoardVO vo = null;
+//		String sql = "SELECT * FROM toon_board WHERE bd_num=?";
+//		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//			pstmt.setInt(1, bId);
+//			ResultSet rs = pstmt.executeQuery();
+//
+//			if (rs.next()) {
+//				vo = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
+//						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
+//						rs.getInt("bd_view"));
+//			}
+//		} catch (Exception e) {
+//
+//		}
+//		return vo;
+//	}
 
+	
 	@Override
 	public void updateBoard(String bd_title, String bd_content, String absoluteImgPath, int bId) {
-
-		String sql = "UPDATE toon_board SET " + "bd_title=?, bd_content=?,bd_img=? " + "WHERE bd_num=?";
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setString(1, bd_title);
-			pstmt.setString(2, bd_content);
-			pstmt.setString(3, absoluteImgPath);
-			pstmt.setInt(4, bId);
-			pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		boardMapper.updateBoard(bd_title, bd_content, absoluteImgPath, bId);
 	}
-
+		
+//	@Override
+//	public void updateBoard(String bd_title, String bd_content, String absoluteImgPath, int bId) {
+//
+//		String sql = "UPDATE toon_board SET " + "bd_title=?, bd_content=?,bd_img=? " + "WHERE bd_num=?";
+//		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//			pstmt.setString(1, bd_title);
+//			pstmt.setString(2, bd_content);
+//			pstmt.setString(3, absoluteImgPath);
+//			pstmt.setInt(4, bId);
+//			pstmt.executeUpdate();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
 	@Override
 	public void deleteBoard(int bId) {
-
-		String sql = "DELETE FROM toon_board WHERE bd_num=?";
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setInt(1, bId);
-			pstmt.executeUpdate();
-			System.out.println("deleteBoard실행");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+		boardMapper.deleteBoard(bId);
 	}
+
+//	@Override
+//	public void deleteBoard(int bId) {
+//
+//		String sql = "DELETE FROM toon_board WHERE bd_num=?";
+//		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//			pstmt.setInt(1, bId);
+//			pstmt.executeUpdate();
+//			System.out.println("deleteBoard실행");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
 
 	@Override
 	public List<BoardVO> searchBoard(String search, String category) {
-
-		List<BoardVO> articles = new ArrayList<>();
-		String sql = null;
-		switch (category) {
-		case "제목":
-			sql = "SELECT * FROM toon_board WHERE bd_title LIKE ? AND bd_email !='root@naver.com'";
-			break;
-		case "작성자":
-			sql = "SELECT * FROM toon_board WHERE bd_writer LIKE ? AND bd_email !='root@naver.com'";
-			break;
-		default:
-			sql = "SELECT * FROM toon_board WHERE bd_email !='root@naver.com'";
-		}
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setString(1, "%" + search + "%");
-			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				BoardVO article = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
-						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
-						rs.getInt("bd_view"));
-				articles.add(article);
-			}
-		} catch (Exception e) {
-
-		}
-		return articles;
+		return boardMapper.searchBoard(search, category);
 	}
+	
+//	@Override
+//	public List<BoardVO> searchBoard(String search, String category) {
+//
+//		List<BoardVO> articles = new ArrayList<>();
+//		String sql = null;
+//		switch (category) {
+//		case "제목":
+//			sql = "SELECT * FROM toon_board WHERE bd_title LIKE ? AND bd_email !='root@naver.com'";
+//			break;
+//		case "작성자":
+//			sql = "SELECT * FROM toon_board WHERE bd_writer LIKE ? AND bd_email !='root@naver.com'";
+//			break;
+//		default:
+//			sql = "SELECT * FROM toon_board WHERE bd_email !='root@naver.com'";
+//		}
+//		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//			pstmt.setString(1, "%" + search + "%");
+//			ResultSet rs = pstmt.executeQuery();
+//
+//			while (rs.next()) {
+//				BoardVO article = new BoardVO(rs.getInt("bd_num"), rs.getString("bd_writer"), rs.getString("bd_title"),
+//						rs.getString("bd_content"), rs.getString("bd_email"), rs.getDate("bd_date"),
+//						rs.getInt("bd_view"));
+//				articles.add(article);
+//			}
+//		} catch (Exception e) {
+//
+//		}
+//		return articles;
+//	}
 
 	@Override
 	public void viewIncrease(int bId, int bView) {
@@ -215,18 +248,10 @@ public class BoardServiceImpl implements BoardService {
 		return articles;
 	}
 
+	
 	@Override
 	public int getCountBoard() {
-		int count = 0;
-		String sql = "select count(*) from toon_board"; //toon_board에 레코드수 select
-		try (Connection conn = ds.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()){
-				count = rs.getInt(1);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return count; // 총 레코드 수 리턴
+		return boardMapper.getCountBoard();
 	}
+	
 }
