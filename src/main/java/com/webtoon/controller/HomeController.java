edd54eb3d33e0bd27f7e001796a5a9f1beb7a0e9
김민수 <minsu980107@naver.com>
2021-06-03 00:35:49 +0900

    pom에 MappingJacksonJsonView 추가
    pom에 MappingJacksonJsonView 추가

--- src/main/java/com/webtoon/controller/HomeController.java
+++ src/main/java/com/webtoon/controller/HomeController.java
@@ -1,8 +1,9 @@
 package com.webtoon.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.Enumeration;
 import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,8 +24,17 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(HttpSession session,Locale locale, Model model) {
 		
+		
+		////저장된 세션을 모두 보여주는 코드입니다
+		Enumeration<?> attrName = session.getAttributeNames();
+		while (attrName.hasMoreElements()) 
+		{ 
+			String attr = (String) attrName.nextElement();
+			System.out.println(attr+" : "+session.getAttribute(attr)); 
+		}
+		////
 		return "index";
 	}
 	
